a30a700cb80686e28da8fa2d877a6fea1e788ee0
jackdbastian <jackdbastian@gmail.com>
2021-09-26 12:28:27 -0700

    add sodapy support

--- update.py
+++ update.py
@@ -1,28 +1,41 @@
-from boxsdk import JWTAuth, Client
-from boxsdk.network.default_network import DefaultNetwork
 from socrata.authorization import Authorization
-import os
+from socrata import Socrata
+from sodapy import Socrata as Soda
 import json
 import utils
+import os
+# from boxsdk import JWTAuth, Client
+# from boxsdk.network.default_network import DefaultNetwork
 
-#box_auth = JWTAuth.from_settings_file('jwt_auth.json')
-#box_client = Client(box_auth)
-  
+# box_auth = JWTAuth.from_settings_file('jwt_auth.json')
+# box_client = Client(box_auth)
+
+SOCRATA_ID = os.environ['SOCRATA_ID']
+SOCRATA_PASS = os.environ['SOCRATA_KEY']
+APP_TOKEN = os.environ['SOCRATA_APP_TOKEN']
 
 domain = "healthdata.gov"
 
-socrata_auth = Authorization(
+socrata_py_auth = Authorization(
   domain,
-  os.environ['SOCRATA_ID'], 
-  os.environ['SOCRATA_KEY']
+  SOCRATA_ID, 
+  SOCRATA_PASS
+)
+
+socrata_py_client = Socrata(socrata_py_auth)
+
+sodapy_client = Soda(
+    domain,
+    APP_TOKEN,
+    SOCRATA_ID,
+    SOCRATA_PASS
 )
 
 with open('profile_reports_config.json') as f:
   profile_reports = json.load(f)
 
-
 for i in profile_reports:
   try:
-    utils.upload_state_report(i['id'], i['name'], domain, socrata_auth)
+    utils.upload_state_report(i['id'], i['name'], domain, socrata_py_client, sodapy_client, SOCRATA_ID, SOCRATA_PASS)
   except:
     pass
--- utils.py
+++ utils.py
@@ -32,9 +32,8 @@ def add_attachment(revision, file_path, file_name):
         'attachments': attachments
     })
 
-def upload_state_report(dataset_id, state, domain, auth):
+def upload_state_report(dataset_id, state, domain, socrata_py_client, sodapy_client, SOCRATA_ID, SOCRATA_PASS):
     
-    from socrata import Socrata
     import requests
     import os
     import json
@@ -43,9 +42,7 @@ def upload_state_report(dataset_id, state, domain, auth):
 
     meta_url = f"https://{domain}/api/views/metadata/v1/{dataset_id}"
 
-    socrata = Socrata(auth)
-
-    view = socrata.views.lookup(dataset_id)
+    view = socrata_py_client.views.lookup(dataset_id)
 
     revision = view.revisions.create_replace_revision()
 
@@ -54,21 +51,22 @@ def upload_state_report(dataset_id, state, domain, auth):
     filepath = f'input/{state}_State_Profile_Report_{current_date}_Public.pdf' 
 
     filename = f'{state}_State_Profile_Report_{current_date}_Public.pdf'
-    with open(filepath, 'rb') as file:
-        upload = revision.source_as_blob(filename)
-        source = upload.blob(file)
-        job = revision.apply()
-        job.wait_for_finish(progress=lambda job: print(state, 'file upload: ', job.attributes['status']))
+
+    with open(filepath, 'rb') as f:
+        files = (
+            {'file': (filename, f)}
+        )
+        response = sodapy_client.replace_non_data_file(dataset_id, {}, files)
     
+    last_update =  (date.today() - timedelta(4)).strftime("%b %d, %Y")
+    payload = {"customFields": {"Common Core": {"Last Update" : last_update}}}
+    json_data = json.dumps(payload)
+    req_update = requests.patch(meta_url, json_data, auth=(SOCRATA_ID, SOCRATA_PASS))
+    meta_new = req_update.text
+
     
     revision = view.revisions.create_replace_revision()
 
     attachment_update = add_attachment(revision, filepath, filename) 
     job = attachment_update.apply()
     job.wait_for_finish(progress=lambda job: print(state, 'attachment upload: ', job.attributes['status']))   
-    last_update =  (date.today() - timedelta(4)).strftime("%b %d, %Y")
-    payload = {"customFields": {"Common Core": {"Last Update" : last_update}}}
-    json_data = json.dumps(payload)
-    req_update = requests.patch(meta_url, json_data, auth=(os.environ['SOCRATA_ID'],os.environ['SOCRATA_KEY']))
-    meta_new = req_update.text
-    
